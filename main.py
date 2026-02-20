5d5eda8888ef6fef0587fba531310a8a3f9b8587
Aryan Pandhare <53331522+aryan2000sp@users.noreply.github.com>
2026-02-20 15:21:58 -0600

    Created a grid

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 # Example file showing a basic pygame "game loop"
 import pygame
+from settings import *
+from Map import Map
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 running = True
+
+newMap = Map()
 
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
             running = False
+
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
     # RENDER YOUR GAME HERE
+    newMap.draw(screen)
+
 
     # flip() the display to put your work on screen
     pygame.display.flip()

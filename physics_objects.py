ca2ec63eb635ad06d7e1e458efaac7dc71a76b91
Andre <106368574+seigfrieds@users.noreply.github.com>
2026-02-20 20:24:04 -0600

    move to velocity for movement

--- physics_objects.py
+++ physics_objects.py
@@ -6,5 +6,35 @@ class CollidingObject():
         return self.collider
 
 class MovingCollidingObject(CollidingObject):
-    def move():
-        raise NotImplementedError("Implement the method")
+    def __init__(self, x, y, c):
+        self.x = x
+        self.y = y
+        self.velocity_x = 0
+        self.velocity_y = 0
+        super().__init__(c)
+
+    def get_pos(self):
+        return (self.x, self.y)
+    
+    def set_pos(self, new_x, new_y):
+        self.x = new_x
+        self.y = new_y
+    
+    def set_velocity(self, velocity_x, velocity_y):
+        self.velocity_x = velocity_x
+        self.velocity_y = velocity_y
+
+    def move(self):
+        old_x = self.x
+        old_y = self.y
+
+        self.x += self.velocity_x
+        self.y += self.velocity_y
+        self.get_collider().change_collider_position(self.x, self.y)
+
+        def reset():
+            self.x = old_x
+            self.y = old_y
+            self.get_collider().change_collider_position(old_x, old_y)
+
+        return reset
--- player.py
+++ player.py
@@ -6,15 +6,13 @@ from physics_objects import MovingCollidingObject
 
 class Player(MovingCollidingObject):
     def __init__(self):
-        self.x = 250
-        self.y = 250
         self.radius = 3
         self.turn_direction = 0
         self.walk_direction = 0
         self.rotation_angle = 0 * (math.pi / 180)
         self.move_speed = 2.5
         self.rotation_speed = 2 * (math.pi / 180)
-        super().__init__(Collider(self.x, self.y, self.radius, self.radius)) 
+        super().__init__(250, 250, Collider(250, 250, self.radius, self.radius)) 
 
     def update(self):
         keys = pygame.key.get_pressed()
@@ -33,27 +31,10 @@ class Player(MovingCollidingObject):
 
         #rotate
         self.rotation_angle += self.turn_direction * self.rotation_speed
-    
-    #will be called by physics engine
-    def move(self):
-        old_x = self.x
-        old_y = self.y
 
+        #set velocity
         move_step = self.walk_direction * self.move_speed
-
-        new_x = self.x + math.cos(self.rotation_angle) * move_step
-        new_y = self.y + math.sin(self.rotation_angle) * move_step
-        
-        self.x = new_x
-        self.y = new_y
-        self.get_collider().change_collider_position(new_x, new_y) 
-
-        def reset():
-            self.x = old_x
-            self.y = old_y
-            self.get_collider().change_collider_position(old_x, old_y)
-
-        return reset
+        self.set_velocity(math.cos(self.rotation_angle) * move_step, math.sin(self.rotation_angle) * move_step)
 
     def render(self, screen):
-        pygame.draw.circle(screen, "red", (self.x, self.y), self.radius)
+        pygame.draw.circle(screen, "red", (self.get_pos()[0], self.get_pos()[1]), self.radius)
